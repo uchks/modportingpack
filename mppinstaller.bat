7a9a6d6da58a1d6b84c003a3a78c9e404de8d943
Jason Peterson <38664452+uchks@users.noreply.github.com>
2023-07-30 21:00:11 -0500

    Rewrite and Optimize

--- mppinstaller.bat
+++ mppinstaller.bat
@@ -1,50 +1,52 @@
 @echo off
-rem https://ci.tterrag.com/job/BON2/lastSuccessfulBuild/artifact/build/libs/BON-2.4.0.15-all.jar
-rem https://github.com/deathmarine/Luyten/releases/download/v0.5.4_Rebuilt_with_Latest_depenencies/luyten-0.5.4.jar
-rem Thank god for 2Pi <3
-set workdir="%cd%"
-set installBON2=No
-set installLuyten=No
+setlocal EnableExtensions EnableDelayedExpansion
 
-:print
+rem URL Links
+rem https://ci.tterrag.com/job/BON2/lastSuccessfulBuild/artifact/build/libs/BON-2.4.0.15-all.jar
+rem https://github.com/deathmarine/Luyten/releases/download/v0.5.4_Rebuilt_with_Latest_depenencies/luyten-0.5.4.jar|
+
+rem Thank god for 2Pi <3
+
+:setDefaults
+set "installBON2=No"
+set "installLuyten=No"
+set workdir="%cd%"
+
+:menu
 set "input="
 cls
 echo.
 echo Welcome to the Mod Porting Pack Downloader!
-echo ###########################################
+echo.
 echo Type the number in front of the setting to toggle it (or view), type "go" to start downloading:
 echo.
 echo 1. Download Bearded Octo Nemesis 2: %installBON2%
 echo 2. Download Luyten: %installLuyten%
 echo.
-echo The files will be installed to where this file is! Make sure it's in the correct folder!
-echo.
-set /p input="Enter 1, 2 or go: "
+set /p input="Enter Corresponding Number and Hit Enter: "
 
 if "%input%"=="1" (
     if "%installBON2%"=="No" (
-        set installBON2=Yes
+        set "installBON2=Yes"
     ) else (
-        set installBON2=No
+        set "installBON2=No"
     )
-    goto print
+    goto menu
+) else if "%input%"=="2" (
+    if "%installLuyten%"=="No" (
+        set "installLuyten=Yes"
+    ) else (
+        set "installLuyten=No"
+    )
+    goto menu
 ) else (
-        if "%input%"=="2" (
-            if "%installLuyten%"=="No" (
-                set installLuyten=Yes
-            ) else (
-                set installLuyten=No
-            )
-            goto print
-        ) else (
-            goto start
-   )
-  )
- )
+    goto start
 )
 
 :start
+cls
 if "%installBON2%"=="No" if "%installLuyten%"=="No" goto errorNoInstallsSelected
+
 
 cls
 echo.
@@ -53,34 +55,25 @@ echo.
 pause
 
 if "%installBON2%"=="Yes" (
-    goto installBON2
+    echo Downloading BON2...
+    curl https://ci.tterrag.com/job/BON2/lastSuccessfulBuild/artifact/build/libs/BON-2.4.0.15-all.jar --output "BON.jar"
 )
-:L
+
 if "%installLuyten%"=="Yes" (
-    goto installLuyten
+    echo Downloading Luyten...
+    curl -L https://github.com/deathmarine/Luyten/releases/download/v0.5.4_Rebuilt_with_Latest_depenencies/luyten-0.5.4.jar > luyten.jar
 )
-:END
+
 cls
 echo All files downloaded to %cd%
 echo.
 pause
 exit
 
-:installBON2
-cls
-echo Downloading BON2...
-curl https://ci.tterrag.com/job/BON2/lastSuccessfulBuild/artifact/build/libs/BON-2.4.0.15-all.jar --output "BON.jar"
-goto L
-
-:installLuyten
-cls
-echo Downloading Luyten...
-curl -L https://github.com/deathmarine/Luyten/releases/download/v0.5.4_Rebuilt_with_Latest_depenencies/luyten-0.5.4.jar > luyten.jar
-goto END
 
 :errorNoInstallsSelected
 echo.
 echo Error: You didn't select anything to be downloaded! Please set at least one of the two options to Yes!
 echo.
 pause
-goto print
+goto menu
