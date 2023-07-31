a35fef43171ae17963da78b5b282a3231d5e7700
Jason Peterson <38664452+uchks@users.noreply.github.com>
2023-07-30 20:12:58 -0500

    Switch to cURL
    No more breaking licenses, yay!
    Unfortunately, MCP Mapping Viewer and Mappings themselves seem to be down. Luyten4Forge wasn't recommended. So now MPP is down to just two tools. That is until I care about MC modding again.

--- mppinstaller.bat
+++ mppinstaller.bat
@@ -1,16 +1,10 @@
 @echo off
 rem https://ci.tterrag.com/job/BON2/lastSuccessfulBuild/artifact/build/libs/BON-2.4.0.15-all.jar
-rem https://github.com/KevinPriv/Luyten4Forge/releases/download/1.3/Luyten4Forge.1.3.jar
 rem https://github.com/deathmarine/Luyten/releases/download/v0.5.4_Rebuilt_with_Latest_depenencies/luyten-0.5.4.jar
-rem http://bspk.rs/MC/MCPMappingViewer/MCPMappingViewer_1.0.1.jar
 rem Thank god for 2Pi <3
 set workdir="%cd%"
 set installBON2=No
-set installLuyten4Forge=No
 set installLuyten=No
-set installMCPMappingViewer=No
-
-if not exist "%workdir%/resources" goto errorNoDirectory
 
 :print
 set "input="
@@ -21,13 +15,11 @@ echo ###########################################
 echo Type the number in front of the setting to toggle it (or view), type "go" to start downloading:
 echo.
 echo 1. Download Bearded Octo Nemesis 2: %installBON2%
-echo 2. Download Luyten4Forge: %installLuyten4Forge%
-echo 3. Download Luyten: %installLuyten%
-echo 4. Download MCP Mapping Viewer: %installMCPMappingViewer%
+echo 2. Download Luyten: %installLuyten%
 echo.
 echo The files will be installed to where this file is! Make sure it's in the correct folder!
 echo.
-set /p input="Enter 1, 2, 3, 4 or go: "
+set /p input="Enter 1, 2 or go: "
 
 if "%input%"=="1" (
     if "%installBON2%"=="No" (
@@ -37,27 +29,11 @@ if "%input%"=="1" (
     )
     goto print
 ) else (
-    if "%input%"=="2" (
-        if "%installLuyten4Forge%"=="No" (
-            set installLuyten4Forge=Yes
-        ) else (
-            set installLuyten4Forge=No
-        )
-        goto print
-    ) else (
-        if "%input%"=="3" (
+        if "%input%"=="2" (
             if "%installLuyten%"=="No" (
                 set installLuyten=Yes
             ) else (
                 set installLuyten=No
-            )
-            goto print
-        ) else (
-                if "%input%"=="4" (
-            if "%installMCPMappingViewer%"=="Yes" (
-                set installMCPMappingViewer=No
-            ) else (
-                set installMCPMappingViewer=Yes
             )
             goto print
         ) else (
@@ -68,7 +44,7 @@ if "%input%"=="1" (
 )
 
 :start
-if "%installBON2%"=="No" if "%installLuyten4Forge%"=="No" if "%installLuyten%"=="No" if "%installMCPMappingViewer%"=="No" goto errorNoInstallsSelected
+if "%installBON2%"=="No" if "%installLuyten%"=="No" goto errorNoInstallsSelected
 
 cls
 echo.
@@ -76,28 +52,16 @@ echo By continuing, you confirm that this Downloader is going to install the fil
 echo.
 pause
 
-if not exist "%workdir%/resources/temp" (
-    mkdir "%workdir%/resources/temp"
-)
-
 if "%installBON2%"=="Yes" (
     goto installBON2
-)
-:L4F
-if "%installLuyten4Forge%"=="Yes" (
-    goto installLuyten4Forge
 )
 :L
 if "%installLuyten%"=="Yes" (
     goto installLuyten
 )
-:MCP
-if "%installMCPMappingViewer%"=="Yes" (
-    goto installMCPMappingViewer
-)
 :END
 cls
-echo All files downloaded to /resources/temp/
+echo All files downloaded to %cd%
 echo.
 pause
 exit
@@ -105,40 +69,18 @@ exit
 :installBON2
 cls
 echo Downloading BON2...
-resources\wget\wget.exe --directory-prefix=resources/temp https://ci.tterrag.com/job/BON2/lastSuccessfulBuild/artifact/build/libs/BON-2.4.0.15-all.jar -q --show-progress
-goto L4F
-
-:installLuyten4Forge
-cls
-echo.
-echo Downloading Luyten4Forge...
-resources\wget\wget.exe --directory-prefix=resources/temp https://github.com/KevinPriv/Luyten4Forge/releases/download/1.3/Luyten4Forge.1.3.jar -q --show-progress
+curl https://ci.tterrag.com/job/BON2/lastSuccessfulBuild/artifact/build/libs/BON-2.4.0.15-all.jar --output "BON.jar"
 goto L
 
 :installLuyten
 cls
 echo Downloading Luyten...
-resources\wget\wget.exe --directory-prefix=resources/temp https://github.com/deathmarine/Luyten/releases/download/v0.5.4_Rebuilt_with_Latest_depenencies/luyten-0.5.4.jar -q --show-progress
-goto MCP
-
-:installMCPMappingViewer
-cls
-echo Downloading MCPMappingViewer...
-resources\wget\wget.exe --directory-prefix=resources/temp http://bspk.rs/MC/MCPMappingViewer/MCPMappingViewer_1.0.1.jar -q --show-progress
+curl -L https://github.com/deathmarine/Luyten/releases/download/v0.5.4_Rebuilt_with_Latest_depenencies/luyten-0.5.4.jar > luyten.jar
 goto END
 
 :errorNoInstallsSelected
 echo.
-echo Error: You didn't select anything to be downloaded! Please set at least one of the five options to Yes!
+echo Error: You didn't select anything to be downloaded! Please set at least one of the two options to Yes!
 echo.
 pause
 goto print
-
-:errorNoDirectory
-echo.
-echo Error: Couldn't find the folder "/resources/" in current folder!
-echo.
-echo Current working directory: "%cd%"
-echo.
-pause
-exit
